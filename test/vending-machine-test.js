87fa1daf41232df81b2036e511e97a95fadc5f79
Gourab Porey <gourabporey@Gourabs-MacBook-Pro.local>
2023-04-11 15:30:38 +0530

    Refactored testing framework to show details of failed tests

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -2,7 +2,8 @@ const testing = require('../lib/testing.js');
 const vendingMachine = require('../src/vending-machine.js');
 
 const runTestForVendingMachine = function() {
-  testing.assert(vendingMachine.value, 0, "Should be 0");
+  testing.assertEqual(vendingMachine.value, 0, "Should be 0");
+  testing.assertEqual(vendingMachine.value, 4, "Should be 4");
 }
 
 runTestForVendingMachine();
