30d7ad05cb3174f7543b358197ccb77b86a90f06
Gourab Porey <gourabporey@Gourabs-MacBook-Pro.local>
2023-04-11 17:10:15 +0530

    Upgraded function to give denomination of 10 rupee coin

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,13 +1,16 @@
 const determineOptimumNoOfCoins = function(amount) {
   let amountLeft = amount;
 
+  const tenRupeeCoins = Math.floor(amountLeft / 10);
+  amountLeft = amountLeft % 10;
+
   const fiveRupeeCoins = Math.floor(amountLeft / 5);
   amountLeft = amountLeft % 5;
 
   const twoRupeeCoins = Math.floor(amountLeft / 2);
   amountLeft = amountLeft % 2;
 
-  return fiveRupeeCoins + twoRupeeCoins + amountLeft;
+  return tenRupeeCoins + fiveRupeeCoins + twoRupeeCoins + amountLeft;
 }
 
 const dispenseCoins = function(amount) {
