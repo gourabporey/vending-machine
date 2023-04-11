0d87a27ea5d062a31a0e5abccb38137c08fa6cbb
Gourab Porey <gourabporey@Gourabs-MacBook-Pro.local>
2023-04-11 16:47:49 +0530

    Upgraded function to give denomination of 2 rupee coin also

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,11 @@
+const determineOptimumNoOfCoins = function(amount) {
+  const twoRupeeCoins = Math.floor(amount / 2);
+  const amountLeft = amount % 2;
+  return twoRupeeCoins + amountLeft;
+}
+
 const dispenseCoins = function(amount) {
-  return amount;
+  return determineOptimumNoOfCoins(amount);
 }
 
 exports.dispenseCoins = dispenseCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -3,8 +3,8 @@ const vendingMachine = require('../src/vending-machine.js');
 
 const runTestForVendingMachine = function() {
   testing.assertEqual(vendingMachine.dispenseCoins(0), 0, "Should give nothing for amount of Rs.0");
-  testing.assertEqual(vendingMachine.dispenseCoins(1), 1, "Should give 1 coin for amount of Rs.1");
-  testing.assertEqual(vendingMachine.dispenseCoins(2), 2, "Should give 2 coins for amount of Rs.2");
+  testing.assertEqual(vendingMachine.dispenseCoins(1), 1, "Should give 1 coin of 1 rupee for amount of Rs.1");
+  testing.assertEqual(vendingMachine.dispenseCoins(2), 1, "Should give 1 coin of 2 rupee for amount of Rs.2");
 }
 
 runTestForVendingMachine();
