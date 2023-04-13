ac3994e3c6010897ea3a4d0de759726cb20d2429
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-13 17:15:35 +0530

    Refactored testing framework to return the result of assertions

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -9,9 +9,16 @@ const dispenseCoinsByDenomination = vendingMachine.dispenseCoinsByDenomination;
 
 const runTestForVendingMachine = function() {
   testing.printHeadline("Testing for vending machine");
-  assertEqual(dispenseCoins(1, []), 0, "Should give nothing for no denomination");
-  assertEqual(dispenseCoins(0, [1,2]), 0, "Should give nothing for amount of Rs.0");
-  assertEqual(dispenseCoins(1, [1]), 1, "Should give 1 coin of 1 rupee for amount of Rs.1 and denomination of 1");
+
+  const testMsgForNoDenomination = "Should give nothing for no denomination";
+  assertEqual(dispenseCoins(1, []), 0, testMsgForNoDenomination);
+
+  const testMsgForZeroAmount = "Should give nothing for amount of Rs.0";
+  assertEqual(dispenseCoins(0, [1]), 0, testMsgForZeroAmount );
+
+  const testMsgForOneRupee = "Should give 1 coin of 1 rupee for amount of Rs.1 and denomination of 1";
+  assertEqual(dispenseCoins(1, [1]), 1, testMsgForOneRupee);
+
   assertEqual(dispenseCoins(2, [1,2]), 1, "Should give 1 coin of 2 rupee for amount of Rs.2 and denomination of 1, 2 ");
   assertEqual(dispenseCoins(5, [1,2,5]), 1, "Should give 1 coin of 5 rupee for amount of Rs.5 and denomination of 1, 2, 5");
   assertEqual(dispenseCoins(10, [2,5,10]), 1, "Should give 1 coin of 10 rupee for amount of Rs.10 and denomination of 2, 5, 10");
