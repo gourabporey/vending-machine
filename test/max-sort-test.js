79ae9d799a732333d55e8624a4aadfce67a8ffc6
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-13 23:30:00 +0530

    Added test cases for sorting utility

--- test/max-sort-test.js
+++ test/max-sort-test.js
@@ -6,9 +6,13 @@ const sort = maxSort.sort;
 
 const runTestsForMaxSort = function() {
   testing.printHeadline("Testing for MaxSort");
-  assertArrayEqual(sort([1]), [1], "Should give [1] for input [1]");
-  assertArrayEqual(sort([1, 2]), [1, 2], "Should give [1, 2] for input [1, 2]");
-  assertArrayEqual(sort([1, 3, 2]), [1, 2, 3], "Should give [1, 2, 3] for input [1, 3, 2]");
+  assertArrayEqual(sort([]), [], "Should give empty array for array of no element");
+  assertArrayEqual(sort([1]), [1], "Should give same array for array of one element");
+  assertArrayEqual(sort([1, 2]), [1, 2], "Should give a sorted array for array of two elements");
+  assertArrayEqual(sort([2, 2, 2]), [2, 2, 2], "Should give same array for array of same elements");
+  assertArrayEqual(sort([1, 3, 2, 5, 4]), [1, 2, 3, 4, 5], "Should give a sorted array for array of multiple elements");
+  assertArrayEqual(sort([1, 3, 2, 2, 4]), [1, 2, 2, 3, 4], "Should give a sorted array for array with repeated elements");
+  assertArrayEqual(sort([1, -3, -2, 2, 4]), [-3, -2, 1, 2, 4], "Should give a sorted array for array with mix of +ve & -ve elements");
 }
 
 runTestsForMaxSort();
