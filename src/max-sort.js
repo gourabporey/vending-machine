3023abda592173f341becfc5ba3c08a717ab3f12
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-13 13:04:05 +0530

    Refactored maxSort by saving memory using splice

--- src/max-sort.js
+++ src/max-sort.js
@@ -3,18 +3,13 @@ const maxOf = function(numbers) {
   return Math.max(numbers[0], maxOf(numbers.slice(1)));
 }
 
-const ejectAt = function(element, array) {
-  const index = array.indexOf(element);
-  return array.slice(0,index).concat(array.slice(index + 1, array.length));
-}
-
 const sort = function(numbers) {
   let modifiableArray = numbers.slice(0);
   const sortedArray = [];
 
   for (let i = 0; i < numbers.length; i++) {
     const maxOfCurrentArray = maxOf(modifiableArray);
-    modifiableArray = ejectAt(maxOfCurrentArray, modifiableArray);
+    modifiableArray.splice(modifiableArray.indexOf(maxOfCurrentArray), 1);
     sortedArray.unshift(maxOfCurrentArray);
   }
 
